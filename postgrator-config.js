665a20ec21e01201614d1e466b3341d147dc30cb
Metty Schroeder <mettyj.schroeder@gmail.com>
2020-12-21 15:50:24 -0500

    debuging host error

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
-    // "host": process.env.MIGRATION_DB_HOST,
+    "host": "ec2-3-229-51-131.compute-1.amazonaws.com",
     // "port": process.env.MIGRATION_DB_PORT,
     // "database": process.env.MIGRATION_DB_NAME,
     // "username": process.env.MIGRATION_DB_USER,
