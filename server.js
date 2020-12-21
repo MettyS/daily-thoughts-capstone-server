04007997ce05b573b9372b96315804b4d9c40565
Metty Schroeder <mettyj.schroeder@gmail.com>
2020-12-21 15:32:39 -0500

    updating dependency

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ const db = knex({
 app.set('db', db)
 
 app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
+    console.log(`Server listening at ${DATABASE_URL}:${PORT}`)
 })
