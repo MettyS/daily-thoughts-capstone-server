5e5a483a188546d9333983adac8a083e4cb74a36
Metty Schroeder <mettyj.schroeder@gmail.com>
2020-12-21 16:13:15 -0500

    adding logs

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 
+console.log(process.env)
+
 module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
-    "host": "ec2-3-229-51-131.compute-1.amazonaws.com",
+    // "host": "ec2-3-229-51-131.compute-1.amazonaws.com",
     // "port": process.env.MIGRATION_DB_PORT,
     // "database": process.env.MIGRATION_DB_NAME,
     // "username": process.env.MIGRATION_DB_USER,
