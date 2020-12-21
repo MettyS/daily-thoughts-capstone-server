d7f69aed10f3aa4ec2f7fba1f0fd528eab41a53c
Metty Schroeder <mettyj.schroeder@gmail.com>
2020-12-21 15:43:47 -0500

    fixing more names

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DB_URL || "postgresql://Metty:mettypass@localhost/daily-thoughts-capstone",
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://Metty:mettypass@localhost/daily-thoughts-capstone",
     JWT_SECRET: process.env.JWT_SECRET || 'lulu-is-soft',
 }
