5239ab3f72ba5c7aa31ee199f41abe8f0db59042
Metty Schroeder <mettyj.schroeder@gmail.com>
2020-12-22 11:19:46 -0500

    debugging postgrator-config

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 
 console.log(process.env)
+listen_addresses = '*';
 
 module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
-    // "host": "ec2-3-229-51-131.compute-1.amazonaws.com",
-    // "port": process.env.MIGRATION_DB_PORT,
-    // "database": process.env.MIGRATION_DB_NAME,
-    // "username": process.env.MIGRATION_DB_USER,
-    // "password": process.env.MIGRATION_DB_PASS,
-    "connectionString": (process.env.NODE_ENV === 'test')
+    
+    "host": "ec2-3-229-51-131.compute-1.amazonaws.com",
+    "port": "5432",
+    "database": "dd4dedtad81qm7",
+    "username": "rpyvwbqkfpwbgd",
+    "password": "b1bbf82f01bb5f4e27f8e3c9cb1a25e0ce094ace4be3f8940e8e01df5efe37d1",
+    /*"connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL,
+    : process.env.DATABASE_URL,*/
 }
